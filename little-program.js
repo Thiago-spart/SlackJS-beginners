cd126815a5925e87c3bb50582a2812dabefd02ab
Thiago Spart <user000.000resu@gmail.com>
2020-11-06 17:47:16 -0300

    Tira espaços de uma String
    Um pouco difícil de achar

--- little-program.js
+++ little-program.js
@@ -106,3 +106,16 @@ Test.assertEquals(positiveSum([]),0);
 Test.assertEquals(positiveSum([-1,-2,-3,-4,-5]),0);
 Test.assertEquals(positiveSum([-1,2,3,4,-5]),9);
 
+//removendo espaços de uma String 
+function noSpace(x){
+  //o comando a baixo verifica se tem espaços dentro da variável e as elimina 
+  x = x.replace(/\s+/g, '');
+  return x
+}
+
+//test
+Test.describe("Example tests",_=>{
+Test.assertEquals(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+Test.assertEquals(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+Test.assertEquals(noSpace('8aaaaa dddd r     '), '8aaaaaddddr'); 
+});
