4b66f82a748032fa7c72c28609b87686f485d96c
Thiago Spart <user000.000resu@gmail.com>
2020-11-06 17:55:31 -0300

    Atv 3
    transforma os números em um r que retorna a soma deles ao quadrado

--- little-program.js
+++ little-program.js
@@ -119,3 +119,17 @@ Test.assertEquals(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8
 Test.assertEquals(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd'); 
 Test.assertEquals(noSpace('8aaaaa dddd r     '), '8aaaaaddddr'); 
 });
+
+//soma todos os números ao quadrado(vezes ele msm)
+function squareSum(numbers){
+  let r = 0
+  for (var i = 0; i < numbers.length; i++) {   
+   r += numbers[i] * numbers[i]
+  }
+  return r
+}
+
+//test
+Test.assertEquals(squareSum([1,2]), 5)
+Test.assertEquals(squareSum([0, 3, 4, 5]), 50)
+
